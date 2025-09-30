b7fa57be1198961832609a7bce6d0c3c479f7033
shennawy <ahmedshennawy997@gmail.com>
2025-09-30 15:16:14 +0300

    first commit

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <section className="container-fluid text-center">
+        <h1>welcome to the cafe</h1>
+      </section>
+    </>
+  );
+}
